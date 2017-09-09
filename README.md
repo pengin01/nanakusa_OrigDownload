53ee609704839e65a838d9a9b97f3c22672f8b8a
zawa <shiro83@gmail.com>
2017-09-09 09:29:12 +0900

    タブ閉じるボタン追加

--- README.md
+++ README.md
@@ -6,3 +6,41 @@
 + chromeの拡張機能を開いて「デベロッパーモード」にチェック
 + 「パッケージされていない〜」を押して、解凍したnanakusa.zipを読み込む
 + ツールバーにアイコンが現れるので押す
+
+### アプリの構成
+```
+.
+├── README.md
+├── background.js ---拡張機能の動作（ここからmain.htmlを呼び出している）拡張機能がインストールされてから裏で動き続けている。
+├── bootstrap ---この配下は画面を今風にするライブラリ
+│   ├── css
+│   │   ├── bootstrap-grid.css
+│   │   ├── bootstrap-grid.css.map
+│   │   ├── bootstrap-grid.min.css
+│   │   ├── bootstrap-grid.min.css.map
+│   │   ├── bootstrap-reboot.css
+│   │   ├── bootstrap-reboot.css.map
+│   │   ├── bootstrap-reboot.min.css
+│   │   ├── bootstrap-reboot.min.css.map
+│   │   ├── bootstrap.css
+│   │   ├── bootstrap.css.map
+│   │   ├── bootstrap.min.css
+│   │   └── bootstrap.min.css.map
+│   └── js
+│       ├── bootstrap.js
+│       └── bootstrap.min.js
+├── icons ---kawaiiアイコン
+│   ├── icon_128.png
+│   └── icon_16.png
+├── lib ---使用しているライブラリ
+│   └── jquery-3.2.1.min.js
+├── main.html ---メインページ
+├── main.js ---メインページの処理
+└── manifest.json ---クローム拡張機能の設定
+```
+
+### 参考文献
++ Chrome JavaScript APIs　https://developer.chrome.com/extensions/api_index
++ Google Chrome Extensionの作り方 ～実践編～ https://risaiku.net/archives/1686/
++ ファイルをダウンロードさせるには？［JavaScript］http://www.atmarkit.co.jp/ait/articles/1603/30/news026.html
++ とほほのBootstrap入門 http://www.tohoho-web.com/ex/bootstrap.html
--- main.js
+++ main.js
@@ -1,34 +1,42 @@
 var cnt = 1;
 
-$(function (){
+
+$(function() {
   // 初期表示
   tab_view();
 
-  // 更新クリック
-  $('#view_reload').click(function(){
+  // 更新ボタンクリック
+  $('#view_reload').click(function() {
     location.reload();
     return false;
   });
-  // 一括ダウンロードクリック
-  $('#all_download').click(function(){
+  // 一括ダウンロードボタンクリック
+  $('#all_download').click(function() {
     all_download();
+    return false;
+  });
+  // タブ閉じボタンクリック
+  $('#tab_close').click(function() {
+    tab_close();
+    location.reload();
     return false;
   });
 });
 
+// 初期表示
 function tab_view() {
 
   // Window取得
-  chrome.windows.getAll(null, function(windows){
+  chrome.windows.getAll(null, function(windows) {
 
-    for (var i=0; i<windows.length; i++) {
+    for (var i = 0; i < windows.length; i++) {
       var winId = windows[i].id;
 
       // Window.idからWindowのタブを取得
-      chrome.tabs.getAllInWindow(winId, function(tabs){
+      chrome.tabs.getAllInWindow(winId, function(tabs) {
 
         // タブ一覧
-        for (var j=0; j<tabs.length; j++) {
+        for (var j = 0; j < tabs.length; j++) {
 
           // 自分は除外
           if (tabs[j].title == 'Tab Viewer') {
@@ -36,23 +44,24 @@ function tab_view() {
           }
 
           // タブタイトル
-         var title = tabs[j].title;
+          var title = tabs[j].title;
 
-         var extend = ":orig";
-         var url = tabs[j].url;
+          // 不可する文字列
+          var extend = ":orig";
+          var url = tabs[j].url;
 
-         // すでにorigで終わってる場合、なにも追加しない
-         if (":orig" == url.slice(-5)) {
-           extend = "";
-         }
-         url = url + extend;
+          // すでにorigで終わってる場合、なにも追加しない
+          if (":orig" == url.slice(-5)) {
+            extend = "";
+          }
+          url = url + extend;
 
-         var tLst = $("<div class='tgt_item form-group'>"
-                + "<label class='tab_title' for='name'>" + title + "</label>"
-                + "<input type='text' class='tab_url form-control' value=" + url + "></div>");
+          var tLst = $("<div class='tgt_item form-group'>" +
+            "<label class='tab_title' for='name'>" + title + "</label>" +
+            "<input type='text' class='tab_url form-control' value=" + url + "></div>");
 
           // リストに追加
-         $('#tab_list').append(tLst);
+          $('#tab_list').append(tLst);
         }
 
         /// $('#window_list').append($('<li></li>'));
@@ -61,6 +70,34 @@ function tab_view() {
   });
 }
 
+// タブ閉じる処理
+function tab_close() {
+
+  // Window取得
+  chrome.windows.getAll(null, function(windows) {
+
+    for (var i = 0; i < windows.length; i++) {
+      var winId = windows[i].id;
+
+      // Window.idからWindowのタブを取得
+      chrome.tabs.getAllInWindow(winId, function(tabs) {
+
+        // タブ一覧
+        for (var j = 0; j < tabs.length; j++) {
+
+          // 自分は除外
+          if (tabs[j].title == 'Tab Viewer') {
+            continue;
+          }
+          // tabIDを指定してタブを閉じる
+          chrome.tabs.remove(tabs[j].id);
+        }
+      });
+    }
+  });
+}
+
+// download関数
 function all_download() {
   $('.tgt_item').each(function() {
     var filename = $(this).find('.tab_title').text();
@@ -69,11 +106,12 @@ function all_download() {
     if (url == null || url == "") {
       return true;
     }
-
+    // download本題
     core_download(url, filename);
   });
 }
 
+// download本体
 function core_download(url, filename) {
 
   if (url == null || filename == null || url.length <= 5) {
@@ -86,7 +124,7 @@ function core_download(url, filename) {
   var xhr = new XMLHttpRequest();
   xhr.open("GET", url, true);
   xhr.responseType = "blob"; // Blobオブジェクトとしてダウンロードする
-  xhr.onload = function (oEvent) {
+  xhr.onload = function(oEvent) {
 
     // ダウンロード完了後の処理を定義する
     var blob = xhr.response;
